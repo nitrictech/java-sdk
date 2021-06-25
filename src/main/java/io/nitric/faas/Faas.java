274882a0a4a736f7304312fa41e91e69151871df
Tim Holm <tim.holm@nitric.io>
2021-06-25 14:31:59 +1000

    feat(faas): Add WIP Bi-Di streaming implementation.

--- src/main/java/io/nitric/faas/Faas.java
+++ src/main/java/io/nitric/faas/Faas.java
@@ -20,19 +20,19 @@ package io.nitric.faas;
  * #L%
  */
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.io.InvalidObjectException;
-import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
-import io.nitric.proto.faas.v1.TriggerRequest;
-import com.google.protobuf.util.JsonFormat;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import io.nitric.proto.faas.v1.FaasGrpc;
+import io.nitric.proto.faas.v1.InitRequest;
+import io.nitric.proto.faas.v1.ServerMessage;
+import io.nitric.proto.faas.v1.ClientMessage;
 
 /**
  * <p>
@@ -106,45 +106,73 @@ public class Faas {
      */
     public void startFunction(NitricFunction function) {
         Objects.requireNonNull(function, "null function parameter");
+        // TODO: Add constants for membrane server
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).build();
+        var svc = FaasGrpc.newStub(channel);
+        AtomicReference<StreamObserver<ClientMessage>> clientObserver = new AtomicReference<>();
 
-        if (httpServer != null) {
-            throw new IllegalStateException("server already started");
-        }
+        // Add a latch to block on while the stream is running
+        CountDownLatch finishedLatch = new CountDownLatch(1);
+        // Begin the stream
+        var observer = svc.triggerStream(new StreamObserver<ServerMessage>() {
+            @Override
+            public void onNext(ServerMessage serverMessage) {
+                // We got a new message from the server
+                switch(serverMessage.getContentCase()) {
+                    case INIT_RESPONSE:
+                        // We have an init ack from the membrane
+                        // XXX: NO OP for now
+                        break;
+                    case TRIGGER_REQUEST:
+                        var trigger = Trigger.buildTrigger(serverMessage.getTriggerRequest());
+                        // Call the function
+                        var response = function.handle(trigger);
+                        var grpcResponse = response.toGrpcTriggerResponse();
+                        // write back the response to the server
+                        clientObserver.get().onNext(
+                                ClientMessage
+                                        .newBuilder()
+                                        .setId(serverMessage.getId())
+                                        .setTriggerResponse(grpcResponse)
+                                        .build());
+                        break;
+                    case CONTENT_NOT_SET:
+                        // TODO: Add error case here
+                        break;
+                }
+            }
 
-        var childAddress = System.getenv("CHILD_ADDRESS");
-        if (childAddress != null && !childAddress.isBlank()) {
-            hostname = childAddress;
-        }
+            @Override
+            public void onError(Throwable throwable) {
+                // TODO: Handle stream errors here
+                // We may want to exit when the membrane server indicates an error...
+            }
 
+            @Override
+            public void onCompleted() {
+                // The server has indicated that streaming is now over we can exit
+                // Unlock from exit
+                finishedLatch.countDown();
+            }
+        });
+
+        // Set atomic reference for the client to send messages back to the server
+        // In the server message stream observer loop (see above)
+        clientObserver.set(observer);
+        // Send an init request to the server and let it know we're ready to receive work
+        observer.onNext(
+            ClientMessage
+                    .newBuilder()
+                    .setInitRequest(InitRequest.newBuilder().build())
+                    .build()
+        );
         try {
-            httpServer = HttpServer.create(new InetSocketAddress(hostname, port), 0);
-
-            httpServer.createContext("/", buildServerHandler(function));
-
-            httpServer.setExecutor(null);
-
-            // Start the server
-            httpServer.start();
-
-            var builder = new StringBuilder().append(getClass().getSimpleName());
-            if (DEFAULT_HOSTNAME.equals(hostname)) {
-                builder.append(" listening on port ").append(port);
-
-            } else {
-                builder.append(" listening on ").append(hostname).append(":").append(port);
-            }
-            builder.append(" with function: ");
-
-            if (!function.getClass().getSimpleName().isEmpty()) {
-                builder.append(function.getClass().getSimpleName());
-            } else {
-                builder.append(function.getClass().getName());
-            }
-
-            System.out.println(builder);
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            finishedLatch.await();
+        } catch (Throwable t) {
+            // Log that the stream was prematurely terminated
+        } finally {
+            // Always ensure the client stream is closed
+            observer.onCompleted();
         }
     }
 
@@ -158,77 +186,4 @@ public class Faas {
         faas.startFunction(function);
         return faas;
     }
-
-    // Package Private Methods ------------------------------------------------
-
-    HttpHandler buildServerHandler(final NitricFunction function) {
-
-        return new HttpHandler() {
-
-            /**
-             * Implements the JDK HTTP server handler.
-             *
-             * @param he the HTTP exchange object
-             * @throws IOException if an I/O error occurs
-             */
-            @Override
-            public void handle(HttpExchange he) throws IOException {
-
-                try {
-                    if (he.getRequestBody() == null) {
-                        // Invalid request from membrane
-                        // TODO: Likely need to change this to a more valid exception type
-                        throw new InvalidObjectException("Membrane did not send a trigger request");
-                    }
-
-                    var trBuilder = TriggerRequest.newBuilder();
-                    // Deserialize the requset from the membrane
-                    var jsonString = new String(he.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
-                    JsonFormat.parser().ignoringUnknownFields().merge(jsonString, trBuilder);
-                    var triggerRequest = trBuilder.build();
-
-                    var trigger = Trigger.buildTrigger(triggerRequest);
-
-                    Response response = null;
-
-                    try {
-                        response = function.handle(trigger);
-                    } catch (Throwable t) {
-                        // Return default response type back to the membrane with failure indicators
-                        response = trigger.buildResponse(
-                            "An error occurred, please see logs for details.\n".getBytes(StandardCharsets.UTF_8)
-                        );
-                        if (response.getContext().isHttp()) {
-                            response.getContext().asHttp().setStatus(500);
-                            response.getContext().asHttp().addHeader("Content-Type", "text/plain");
-                        }
-                    }
-
-                    var triggerResponse = response.toGrpcTriggerResponse();
-                    var jsonResponse = JsonFormat.printer().print(triggerResponse);
-
-                    he.getResponseHeaders().put("Content-Type", Collections.singletonList("application/json"));
-                    he.sendResponseHeaders(200, jsonResponse.length());
-
-                    he.getResponseBody().write(jsonResponse.getBytes(StandardCharsets.UTF_8));
-                    he.getResponseBody().close();
-
-                } catch (Throwable t) {
-                    // Log error
-                    System.err.printf("Error occurred handling request %s %s with function: %s \n",
-                            he.getRequestMethod(),
-                            he.getRequestURI(),
-                            function.getClass().getName());
-                    t.printStackTrace();
-
-                    // Write HTTP error response
-                    var msg = "An error occurred, please see logs for details.\n";
-                    he.sendResponseHeaders(500, msg.length());
-                    he.getResponseBody().write(msg.getBytes(StandardCharsets.UTF_8));
-                    he.getResponseBody().close();
-                }
-            }
-        };
-    }
-
 }
